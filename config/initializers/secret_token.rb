cd3e35e5703773c5edcc008d41174fbc0b18b676
RobertStevenson <robert.stevenson@gmail.com>
2010-10-04 12:29:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YellowMushroom::Application.config.secret_token = 'f108ef25b3feb0d71d741685f64a5a3aa28c5f4b0dbecd12c0a1c50a4e505621e556a34f66138c4cfb2bfb66796b9e4eea1939601288d45578b24c5f0d35f861'
